f04b9307392f508faf7b79d68c1a2fb7b70f3d3e
sshiuan2 <sshiuan2@gmail.com>
2013-07-27 05:56:40 -0700

    hi

---
+++ git_add_files.sh
@@ -0,0 +1,8 @@
+git add src/java/controller/LoginAction.java
+git add src/java/controller/LoginForm.java
+git add src/java/controller/RegisterAction.java
+git add src/java/controller/RegisterForm.java
+git add web/
+
+git add git_add_files.sh
+git add git_upload.sh
