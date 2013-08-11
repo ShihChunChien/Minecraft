ac31ad52348de2deb7275536eea2ef6ac0878948
sshiuan2 <sshiuan2@gmail.com>
2013-08-10 18:54:22 -0700

    ok2

--- git_add_files.sh
+++
@@ -1,9 +0,0 @@
-git add git_add_files.sh
-git add git_upload.sh
-
-git add src/java/controller/LoginAction.java
-git add src/java/controller/LoginForm.java
-git add src/java/controller/RegisterAction.java
-git add src/java/controller/RegisterForm.java
-git add src/java/controller/encryption.java
-git add web/
---
+++ src/java/controller/Encryption.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Vincent
+ */
+public class Encryption {
+
+    public static String outmd5pwd;
+
+    public static String md5(String pwdin) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(pwdin.getBytes());
+            byte b[] = md.digest();
+            int i;
+            StringBuilder buf = new StringBuilder("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0) {
+                    i += 256;
+                }
+                if (i < 16) {
+                    buf.append("0");
+                }
+                buf.append(Integer.toHexString(i));
+            }
+
+//System.out.println("result: " + buf.toString());
+            outmd5pwd = buf.toString();
+//System.out.println(outmd5pwd);
+        } catch (NoSuchAlgorithmException e) {
+// TODO Auto-generated catch block 
+            e.printStackTrace();
+        }
+        return outmd5pwd;
+    }
+}
